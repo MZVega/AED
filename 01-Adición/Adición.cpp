4ceb4f5c453737758ad0f5ec2665805c3e2d9602
MZVega <49378147+MZVega@users.noreply.github.com>
2019-04-24 18:53:02 -0500

    Add files via upload

---
+++ 01-Adición/Adición.cpp
@@ -0,0 +1,16 @@
+/*Adición
+Programa que permite sumar dos números
+Melissa Zoe Vega - Curso K1051
+Última modificación 20190423
+*/
+
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cin >> a;
+	int b;
+	std::cin >> b;
+	std::cout << a + b;
+}
